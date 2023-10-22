08ca6b66f77960ab58f35a9592b42e35519be76d
cursosdepier <pier_849@hotmail.com>
2023-10-21 19:49:21 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - prints Programming is like building a multilingual puzzle
  * Descripción:followed by a new line.
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
