89810073ccd6e481eefc22a61bf58c598fd75150
cursosdepier <pier_849@hotmail.com>
2023-10-25 17:33:12 -0700

    imprimir todos los números de un dígito

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all single digit numbers of base
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
